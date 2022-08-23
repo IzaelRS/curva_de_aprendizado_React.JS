11189a8b21be39c0fb2571c133958b9fe6be7316
Izael R Silva <87791471+IzaelRS@users.noreply.github.com>
2022-08-22 22:17:50 -0300

    Add files via upload

---
+++ Hello_Word/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hello word react.js</h1>
+      <h2> Emmet funcionou</h2>
+    </div>
+  );
+}
+
+export default App;
